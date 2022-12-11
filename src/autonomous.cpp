1d1d73c2a28f659c13bd048110386e5ad4e8c298
Michael Zheng <zhengm58@gmail.com>
2022-12-10 18:10:10 -0700

    sus

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -12,37 +12,19 @@
  * from where it left off.
  */
 void autonomous() {
-    roller.move_voltage(8000);
-
-    pros::delay(400);
-
     frontLeft.move_voltage(3000);
 	backLeft.move_voltage(3000);
     frontRight.move_voltage(3000);
 	backRight.move_voltage(3000);
 
-    pros::delay(1000);
-
-    roller.brake();
-
-    pros::delay(1000);
+    pros::delay(2000);
 
     frontLeft.brake();
 	backLeft.brake();
     frontRight.brake();
 	backRight.brake();
 
-    frontLeft.move_voltage(-6000);
-	backLeft.move_voltage(-6000);
-    frontRight.move_voltage(-6000);
-	backRight.move_voltage(-6000);
-
-    pros::delay(400);
-
-    frontLeft.brake();
-	backLeft.brake();
-    frontRight.brake();
-	backRight.brake();
-
-    roller.brake();
+    intake.move_voltage(-12000);
+    pros::delay(2000);
+    intake.brake();
 }
