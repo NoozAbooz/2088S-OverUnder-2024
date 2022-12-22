fe131e4b4596ecc75aaafa5534df701b059de19d
Michael Zheng <44128563+NoozSBC@users.noreply.github.com>
2022-12-22 10:51:12 -0700

    basic controler code

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,72 @@
 #include "main.h"
 
-int driveMode = 1;
-
 void opcontrol() {
+	auto timeFlag=pros::millis();
 	while(true) {
-    	//if(controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
-      	//	if(driveMode == 1) {
-        //		driveMode = 0;
-        //		controller.rumble(".");
-		//		controller.set_text(0, 0, "Drive DISABLED");
-		//		pros::delay(50);
-      	//	} else if(driveMode == 0) {
-        //		driveMode = 1;
-        //		controller.rumble("-");
-		//		controller.set_text(0, 0, "DRIVE ENABLED");
-		//		pros::delay(50);
-      	//	}
-    	//}
-
+		if(pros::millis()-timeFlag>=1000) {
+			controller.print(1, 0, "test");
+            timeFlag=pros::millis();
+        }
+		
 		// Drive
-		if(driveMode == 1) {
-    		int power = controller.get_analog(ANALOG_LEFT_Y);
-    		int turn = controller.get_analog(ANALOG_RIGHT_X);
-    		int left = (power - turn) * (12000 / 127);
-    		int right = (power + turn) * (12000 / 127);
-    		frontLeft.move_voltage(left);
-			backLeft.move_voltage(left);
-    		frontRight.move_voltage(right);
-			backRight.move_voltage(right);
+    	int power = controller.get_analog(ANALOG_LEFT_Y);
+    	int turn = controller.get_analog(ANALOG_RIGHT_X);
+    	int left = (power - turn) * (12000 / 127);
+    	int right = (power + turn) * (12000 / 127);
+    	frontLeft.move_voltage(left);
+		backLeft.move_voltage(left);
+    	frontRight.move_voltage(right);
+		backRight.move_voltage(right);
 
-			// Individual button code
-      		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
-      			intake.move_voltage(12000);
-			} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
-			    intake.brake();
-			}
-
-			if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
-      			intake.move_voltage(-12000);
-			} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
-			    intake.brake();
-			}
-
-			if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
-      			flywheel.move_voltage(12000);
-			} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-				flywheel.brake();
-      		}
-
-			if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-      			flywheel.move_voltage(-12000);
-			} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
-				flywheel.brake();
-      		}
-
-			if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
-      			roller.move_voltage(6000);
-			} else {
-			    roller.brake();
-			}
-
-			if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y)) {
-      			roller.move_voltage(-6000);
-			} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)){
-				roller.brake();
-      		}
-
-			if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
-      			expansion.move_voltage(12000);
-			} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
-				expansion.brake();
-      		}
-
-			if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
-      			expansion.move_voltage(-12000);
-			} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
-				expansion.brake();
-      		}
+		// Individual button code
+     	if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+      		intake.move_voltage(12000);
+		} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+		   intake.brake();
 		}
+
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+      		intake.move_voltage(-12000);
+		} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+		    intake.brake();
+		}
+
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+      		flywheel.move_voltage(12000);
+		} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+			flywheel.brake();
+      	}
+
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+      		flywheel.move_voltage(-12000);
+		} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+			flywheel.brake();
+      	}
+
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
+      		roller.move_voltage(6000);
+		} else {
+		    roller.brake();
+		}
+
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y)) {
+      		roller.move_voltage(-6000);
+		} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)){
+			roller.brake();
+      	}
+
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
+      		expansion.move_voltage(12000);
+		} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+			expansion.brake();
+      	}
+
+		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+      		expansion.move_voltage(-12000);
+		} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
+			expansion.brake();
+      	}
+		
+		pros::Task::delay_until(&timeFlag, 10);
   	}
 }
