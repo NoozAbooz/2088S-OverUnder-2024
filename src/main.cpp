9a51b3179e1c34906c0a482c85f0d1f6728f1e17
Michael Zheng <zhengm58@gmail.com>
2023-01-18 21:32:40 -0700

    grafana

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include "main.h"
 
 void opcontrol() {
+	grafanalib::GUIManager manager;
+	manager.setRefreshRate(100);
+
+	grafanalib::VariableGroup<pros::Motor> drivetrainVars({frontLeft, frontRight, backLeft, backRight});
+	grafanalib::VariableGroup<pros::Motor> subsystemVars({flywheel, expansion});
+
+	drivetrainVars.add_getter("Temperature", &pros::Motor::get_temperature);
+	drivetrainVars.add_getter("Actual Velocity", &pros::Motor::get_actual_velocity);
+	drivetrainVars.add_getter("Voltage", &pros::Motor::get_voltage);
+	drivetrainVars.add_getter("Efficiency", &pros::Motor::get_efficiency);
+
+	subsystemVars.add_getter("Temperature", &pros::Motor::get_temperature);
+	subsystemVars.add_getter("Actual Velocity", &pros::Motor::get_actual_velocity);
+	subsystemVars.add_getter("Voltage", &pros::Motor::get_voltage);
+	subsystemVars.add_getter("Efficiency", &pros::Motor::get_efficiency);
+
+	manager.registerDataHandler(&drivetrainVars);
+	manager.registerDataHandler(&subsystemVars);
+	manager.startTask();
+
 	while(true) {
 		// Drive code
     	int power = controller.get_analog(ANALOG_LEFT_Y);
@@ -60,6 +80,8 @@ void opcontrol() {
 		} else if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
 			expansion.brake();
       	}
+
+		// bind shoot func
 		
 		controller.print(1, 0, "Speed: %lf | Temp: %dC", motorSpeed, flywheelTemp);
 		pros::delay(10);
