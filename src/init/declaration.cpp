e65959d8781b7514c6dd71e9a3ff87d3e1902a07
Michael Zheng <zhengm58@gmail.com>
2023-03-08 22:53:19 -0700

    Update declaration.cpp

--- src/init/declaration.cpp
+++ src/init/declaration.cpp
@@ -5,9 +5,12 @@ pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // Drivetrain
 pros::Motor frontLeft(2, pros::E_MOTOR_GEAR_GREEN, false);
-pros::Motor frontRight(13, pros::E_MOTOR_GEAR_GREEN, true);
 pros::Motor backLeft(3, pros::E_MOTOR_GEAR_GREEN, false);
+pros::Motor frontRight(13, pros::E_MOTOR_GEAR_GREEN, true);
 pros::Motor backRight(14, pros::E_MOTOR_GEAR_GREEN, true);
+
+pros::MotorGroup leftSide({frontLeft, backLeft});
+pros::MotorGroup rightSide({frontRight, backRight});
 
 // Intake
 pros::Motor intake(15, pros::E_MOTOR_GEAR_BLUE, false);
@@ -20,3 +23,11 @@ pros::Motor roller(5, pros::E_MOTOR_GEAR_GREEN, true);
 
 // Expansion
 pros::Motor expansion(19, pros::E_MOTOR_GEAR_RED, false);
+
+lemlib::Drivetrain_t drivetrain {
+    &leftSide, // left drivetrain motors
+    &rightSide, // right drivetrain motors
+    10, // track width
+    3.25, // wheel diameter
+    360 // wheel rpm
+};
