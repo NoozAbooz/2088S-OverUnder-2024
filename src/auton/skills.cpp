5b0a6222f2fa985f29de3ad1211ed68a2533691c
ENC0D3D-WasTaken <105524304+ENC0D3D-WasTaken@users.noreply.github.com>
2024-01-10 10:21:02 -0700

    therory skills

--- src/auton/skills.cpp
+++ src/auton/skills.cpp
@@ -2,4 +2,28 @@
 
 void skills() {
     console.println("Skills run");
+
 }
+
+//chassis.moveTo(0, 0, 60000);
+//chassis.moveTo(-15.291, 41.093, 60000);
+//match load
+//chassis.moveTo(-20.642, 18.157, 60000);
+//chassis.moveTo(-9.939, 9.939, 60000);
+//chassis.moveTo(13.953, 10.703, 60000);
+//chassis.moveTo(43.769, 10.512, 60000);
+//chassis.moveTo(71.674, 10.13, 60000);
+//chassis.moveTo(88.303, 9.748, 60000);
+//chassis.moveTo(95.375, 9.557, 60000);
+//chassis.moveTo(99.962, 40.902, 60000);
+//chassis.moveTo(94.419, 30.581, 60000);
+//chassis.moveTo(72.057, 29.817, 60000);
+//chassis.moveTo(49.312, 34.213, 60000);
+//chassis.moveTo(65.94, 46.063, 60000);
+//chassis.moveTo(85.245, 51.606, 60000);
+//chassis.moveTo(50.841, 61.735, 60000);
+//chassis.moveTo(84.098, 65.558, 60000);
+//chassis.moveTo(48.93, 97.286, 60000);
+//chassis.moveTo(81.804, 80.275, 60000);
+//chassis.moveTo(80.275, 109.327, 60000);
+//chassis.moveTo(103.211, 94.801, 60000);
