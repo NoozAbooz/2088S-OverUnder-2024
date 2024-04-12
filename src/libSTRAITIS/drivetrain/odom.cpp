50e3de8f379e20095b2090cb68d1709e57421747
Michael Zheng <zhengm58@gmail.com>
2024-04-11 22:48:28 -0600

    this distance tracking is not sigma fam

--- src/libSTRAITIS/drivetrain/odom.cpp
+++ src/libSTRAITIS/drivetrain/odom.cpp
@@ -1,4 +1,3 @@
-#include "deviceGlobals.hpp"
 #include "main.h"
 
 using namespace strait;
@@ -15,10 +14,11 @@ double gyro_scale1 = 357.91;
 double gyro_scale2 = 360.2;
 
 double wheel_diameter = 3.25;
-double gear_ratio = (36.0 / 48.0);
-double track_width = 10.75;
+double gear_ratio = (36.0 / 48.0); //maybe change to rpm
+double track_width = 12.5;
+double wheel_circumference = M_PI * wheel_diameter;
 
-double get_imu_heading() {
+double get_imu_rotation() {
 	double rotation1 = inertial.get_rotation();
 	double rotation2 = inertial2.get_rotation();
 
@@ -27,52 +27,45 @@ double get_imu_heading() {
 }
 
 double get_dt_distance_travelled() { 
-	double wheel_circumference = M_PI * wheel_diameter;
 	double avg_position = (leftDrive.at(1).get_position() + rightDrive.at(1).get_position()) / 2;
-	return (avg_position / 360) * wheel_circumference / gear_ratio;
+	return (avg_position / 360) * wheel_circumference * gear_ratio;
 }
 
 double get_dt_heading() {
-	double wheel_circumference = M_PI * wheel_diameter;
+	double left_distance = (leftDrive.at(1).get_position() / 360) * wheel_circumference * gear_ratio;
+	double right_distance = (rightDrive.at(1).get_position() / 360) * wheel_circumference * gear_ratio;
 
-	double left_distance = (leftDrive.at(1).get_position() / 360) * wheel_circumference / gear_ratio;
-	double right_distance = (rightDrive.at(1).get_position() / 360) * wheel_circumference / gear_ratio;
-
-	double heading_in_radians = (left_distance - right_distance) / track_width;
-	return std::fmod(to_deg(heading_in_radians), 360);
+	double heading_in_radians = (right_distance - left_distance) / track_width;
+	return to_deg(heading_in_radians);
 }
 
 void strait::odomThread() {
-	double previous_distance_travelled = 0;
-	double change_in_distance = 0;
-	double heading = 0;
-	double distance_travelled = 0;
+	double avg_heading;
+	double previous_distance_travelled;
+	double delta_distance;
+	double heading;
+	double distance_travelled;
 
 	while (true) {
 		// Find average between dt and imu heading
 		// wrap to [0, 360)
-		heading = fmod((360 - get_imu_heading()) + 90, 360);
-		while (heading < 0) {
-    		heading += 360;
-  		}
-  		while (heading > 360) {
-    		heading -= 360;
-  		}
+		avg_heading = fmod((get_dt_heading() + get_imu_rotation()) / 2, 360);
+		heading = fmod((360 - get_imu_rotation()) + 90, 360);
+		theta = fmod(get_imu_rotation(), 360);
 
-		theta = get_imu_heading();
 		distance_travelled = get_dt_distance_travelled();
-        change_in_distance = distance_travelled - previous_distance_travelled;
+        delta_distance = distance_travelled - previous_distance_travelled;
         
 		// std trig functions are in radians, so we have intermediary conversion to radians
-        x += change_in_distance * std::cos(to_rad(heading));
-        y += change_in_distance * std::sin(to_rad(heading));
+        x += delta_distance * std::cos(to_rad(heading));
+        y +=delta_distance * std::sin(to_rad(heading));
 
         // At the end of the loop, set previous_distance_travelled for the next loop iteration
         previous_distance_travelled = distance_travelled;
 
 		// print for debugging
 		chassis.setPose(x, y, theta);
-		printf("X: %f, Y: %f, Theta: %f, %f\n", x, y, get_dt_heading(), get_imu_heading());
+		printf("X: %f, Y: %f, Theta: %f, %f\n", x, y, get_dt_heading(), get_imu_rotation());
 
         pros::delay(10); // todo
     }
