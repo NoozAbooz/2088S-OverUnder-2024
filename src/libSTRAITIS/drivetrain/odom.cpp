e5e69da968a0f378edc54b976c6d23565b600f56
Michael Zheng <zhengm58@gmail.com>
2024-04-13 11:28:32 -0600

    encapsulate

--- src/libSTRAITIS/drivetrain/odom.cpp
+++ src/libSTRAITIS/drivetrain/odom.cpp
@@ -54,16 +54,16 @@ void strait::odomThread() {
 		theta = fmod(get_imu_rotation(), 360);
 
 		distance_travelled = get_dt_distance_travelled();
-        delta_distance = distance_travelled - previous_distance_travelled;
         
 		if (inertial.get_accel().x > 0.1 || inertial.get_accel().y > 0.1) {
+			delta_distance = distance_travelled - previous_distance_travelled;
 			// std trig functions are in radians, so we have intermediary conversion to radians
         	x += delta_distance * std::cos(to_rad(heading));
         	y += delta_distance * std::sin(to_rad(heading));
-		}
 
-        // At the end of the loop, set previous_distance_travelled for the next loop iteration
-        previous_distance_travelled = distance_travelled;
+			// Set previous_distance_travelled for the next loop iteration
+        	previous_distance_travelled = distance_travelled;
+		}
 
 		// print for debugging
 		chassis.setPose(x, y, theta);
